c269d03ec07289a28a8bc727c18f5e25b58e32e1
Noizeisdead <noizee829@gmail.com>
2023-03-21 09:00:20 +0100

    Added buttons and empty methods

--- Eindopdracht/src/main/java/com/example/eindopdracht/HelloController.java
+++ Eindopdracht/src/main/java/com/example/eindopdracht/HelloController.java
@@ -4,11 +4,44 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
+    public Label DataStructuren;
+    public Label ZoekAlgoritmes;
+    public Label sorteerAlgoritmes;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    void createBinaryTree(){
+
+    }
+    @FXML
+    void createStack(){
+
+    }
+    @FXML
+    void createLinkedList(){
+
+    }
+
+    @FXML
+    void searchAlgorithm1(){
+
+    }
+    @FXML
+    void searchAlgorithm2(){
+
+    }
+    @FXML
+    void sortAlgorithm1(){
+
+    }
+    @FXML
+    void sortAlgorithm2(){
+
     }
 }
