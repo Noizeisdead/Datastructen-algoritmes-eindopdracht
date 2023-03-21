245e86f20c2e59bd2d40a67dc4dc93b474b509fe
Noizeisdead <noizee829@gmail.com>
2023-03-21 14:23:34 +0100

    changed GUI

--- Eindopdracht/src/main/java/com/example/eindopdracht/HelloController.java
+++ Eindopdracht/src/main/java/com/example/eindopdracht/HelloController.java
@@ -2,6 +2,7 @@ package com.example.eindopdracht;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -9,37 +10,32 @@ import javafx.stage.Stage;
 import java.io.*;
 
 public class HelloController {
-    public Label DataStructuren;
+    public Label datastructures;
     public Label ZoekAlgoritmes;
     public Label sorteerAlgoritmes;
     public Button test;
-    @FXML
-    private Label welcomeText;
+    public ChoiceBox choiceBox;
     public FileChooser fileChooser;
-
     public final Stage primaryStage;
+    public BufferedReader reader;
 
     public HelloController(){
         this.primaryStage = new Stage();
         this.fileChooser = new FileChooser();
     }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 
     @FXML
     public void createBinaryTree(){
-
+        System.out.println("Binary tree");
     }
     @FXML
     public void createStack(){
-
+        System.out.println("Stack");
     }
     @FXML
     public void createLinkedList(){
-
+        System.out.println("Linked list");
     }
 
     @FXML
@@ -60,17 +56,38 @@ public class HelloController {
     }
 
     @FXML
-    public void selectFile(){
+    public void selectFile() throws IOException {
         File selectedFile = fileChooser.showOpenDialog(primaryStage);
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(new File(selectedFile.toURI())))) {
+        this.reader = new BufferedReader(new FileReader(new File(selectedFile.toURI())));
 
             String line;
-            while ((line = reader.readLine()) != null)
+            while ((line = this.reader.readLine()) != null)
+
                 System.out.println(line);
 
-        } catch (IOException e) {
-            e.printStackTrace();
+
+    }
+    public void toDatastructure(){
+        String value = (String) choiceBox.getValue();
+        switch (value){
+            case "Stack":
+                createStack();
+                break;
+            case "Binary Tree":
+                createBinaryTree();
+                break;
+            case "Linked List":
+                createLinkedList();
+                break;
         }
+    }
+
+    public void readFile() throws IOException {
+//        System.out.println(reader);
+        String line;
+        while ((line = reader.readLine()) != null)
+
+            System.out.println(line);
     }
 }
