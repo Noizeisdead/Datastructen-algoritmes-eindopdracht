6969127a402f0848864d7a359e5a70a340772db2
Noizeisdead <noizee829@gmail.com>
2023-03-21 10:25:33 +0100

    Added file chooser and BufferedReader

--- Eindopdracht/src/main/java/com/example/eindopdracht/HelloController.java
+++ Eindopdracht/src/main/java/com/example/eindopdracht/HelloController.java
@@ -1,14 +1,28 @@
 package com.example.eindopdracht;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.*;
 
 public class HelloController {
     public Label DataStructuren;
     public Label ZoekAlgoritmes;
     public Label sorteerAlgoritmes;
+    public Button test;
     @FXML
     private Label welcomeText;
+    public FileChooser fileChooser;
+
+    public final Stage primaryStage;
+
+    public HelloController(){
+        this.primaryStage = new Stage();
+        this.fileChooser = new FileChooser();
+    }
 
     @FXML
     protected void onHelloButtonClick() {
@@ -16,32 +30,47 @@ public class HelloController {
     }
 
     @FXML
-    void createBinaryTree(){
+    public void createBinaryTree(){
 
     }
     @FXML
-    void createStack(){
+    public void createStack(){
 
     }
     @FXML
-    void createLinkedList(){
+    public void createLinkedList(){
 
     }
 
     @FXML
-    void searchAlgorithm1(){
+    public void searchAlgorithm1(){
 
     }
     @FXML
-    void searchAlgorithm2(){
+    public void searchAlgorithm2(){
 
     }
     @FXML
-    void sortAlgorithm1(){
+    public void sortAlgorithm1(){
 
     }
     @FXML
-    void sortAlgorithm2(){
+    public void sortAlgorithm2(){
 
+    }
+
+    @FXML
+    public void selectFile(){
+        File selectedFile = fileChooser.showOpenDialog(primaryStage);
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(selectedFile.toURI())))) {
+
+            String line;
+            while ((line = reader.readLine()) != null)
+                System.out.println(line);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
